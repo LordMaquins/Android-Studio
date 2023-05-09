75e0174bf0829f22e2b1c87c7fa3d64c0b39aaa9
LordMark <mv20040908@gmail.com>
2023-05-09 20:16:04 -0300

    Banco de dados comentado

--- AgendaPrincipal/app/src/main/java/com/example/agendaprincipal/MainActivity.java
+++ AgendaPrincipal/app/src/main/java/com/example/agendaprincipal/MainActivity.java
@@ -14,27 +14,43 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    package com.example.agendaprincipal;
 
-        btInicio = findViewById(R.id.cadastro);
-        btInicio.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
 
-                abreSegundaTela();
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-            }
-        });
+    public class MainActivity extends AppCompatActivity {
+
+        Button btInicio;
 
 
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            btInicio = findViewById(R.id.cadastro);
+            btInicio.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    abreSegundaTela();
+
+                }
+            });
+
+
+        }
+        private void abreSegundaTela () {
+            Intent it= new Intent(this, Cadastro.class);
+            startActivityForResult(it,1);
+
+
+        }
     }
-    private void abreSegundaTela () {
-        Intent it= new Intent(this, Cadastro.class);
-        startActivityForResult(it,1);
-
-
-    }
-}
